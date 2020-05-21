5396c19be930d3510a65cefd2b5ce14d0e0f02e9
jengyangkong <jkon0009@student.monash.edu>
2020-05-21 16:20:58 +0800

    Create arm and leg classes

---
+++ src/game/Leg.java
@@ -0,0 +1,15 @@
+package game;
+
+
+public class Leg extends PortableItem{
+	
+
+	public Leg(String name, char displayChar) {
+		super(name, displayChar);
+		
+		int legCount = 2;
+		
+		// TODO Auto-generated constructor stub
+	}
+
+}
