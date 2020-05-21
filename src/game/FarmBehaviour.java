4bfcf2e8f2e80a5b5a729cc09f2336b4b67e3cc1
Yung Sheng <yhoo0005@student.monash.edu>
2020-05-21 13:23:40 +0800

    Added craft, pickup and farm behaviour to game package

---
+++ src/game/FarmBehaviour.java
@@ -0,0 +1,15 @@
+package game;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+
+public class FarmBehaviour implements Behaviour {
+
+	@Override
+	public Action getAction(Actor actor, GameMap map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/game/PickUpBehaviour.java
@@ -0,0 +1,15 @@
+package game;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+
+public class PickUpBehaviour implements Behaviour {
+
+	@Override
+	public Action getAction(Actor actor, GameMap map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
