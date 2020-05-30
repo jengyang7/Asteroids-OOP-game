0b5288fab7bcc82b22c25b2a2a16e4de811a84cb
Yung Sheng <Yung Sheng@192.168.0.182>
2020-05-30 17:20:26 +0800

    Added tick to crop and a crop will be ripe automatically in 20turn if not fertilized.

--- src/game/Crop.java
+++ src/game/Crop.java
@@ -1,6 +1,6 @@
 package game;
-
 import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.Location;
 
 public class Crop extends Ground {
 	private boolean fertilised;
@@ -27,6 +27,7 @@ public class Crop extends Ground {
 		}
 	}
 	
+	//Reduce ripe time by 10 turn
 	public void fertilised() {
 	
 		this.turn -= 10;
@@ -51,5 +52,24 @@ public class Crop extends Ground {
 	public boolean retFood() {
 		return this.food;
 	}
+	
+	
+	//After 20 turn,a crop will be ripe automatically
+	@Override
+	public void tick(Location location) {
+		super.tick(location);
+		
+		if (!this.riped) {
+			this.turn -= 1;
+			if (this.turn <= 0) {
+				this.fertilised = false;
+				this.riped = true;
+				System.out.println(this.toString() + " has riped and ready to be harvested");
+			}
+			
+		}
+		
+	}
+	
 
 }
