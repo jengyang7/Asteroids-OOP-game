4f48fea6ed711b9f334dca5a17c8bfe991855d32
Yung Sheng <Yung Sheng@192.168.0.182>
2020-06-13 14:17:14 +0800

    testing sniper

--- src/game/SniperActorChoice.java
+++ src/game/SniperActorChoice.java
@@ -1,23 +1,26 @@
 package game;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actions;
 import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.Display;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Item;
+import edu.monash.fit2099.engine.Menu;
 
 public class SniperActorChoice extends Action {
 	
 	private static boolean waiting;
-
+	private Menu menu = new Menu();
+	
 	@Override
 	public String execute(Actor actor, GameMap map) {
 		List <Item> item = actor.getInventory();
-		SniperActorList showList = new SniperActorList();
-		showList.execute(actor, map);
-		
+		Actions actions = new Actions();
+		actions.add(new SniperActorList());
 		return null;
 	}
 
